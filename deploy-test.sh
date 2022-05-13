27747c4c992080e7f0f82aa0ecbc58412d4bfa52
John Santamaria <santamar72@gmail.com>
2022-05-13 15:47:27 -0400

    deploy to dind pod

--- deploy-test.sh
+++ deploy-test.sh
@@ -13,7 +13,7 @@ tar -C ./src -zcvf src.tz .
 
 ## step 4 copy the zip file to the container into /var/www/html
 ## does not actually have to go to html we can put it in tmp if we want
-docker cp ./src.tz docker_api_1:/tmp
+docker cp ./src.tz dind:/tmp
 
 ## step 5 ssh into the container, unzip the file & put in the root directory where nginx looks for it 
 ## see php-app/docker/provision/nginx.conf "root" directive
