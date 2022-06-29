eee05fb74a33dc0fd2e45a3ce3a7f32fa3211fb2
John Santamaria <santamar72@gmail.com>
2022-06-28 23:17:03 -0400

    cleaned up docker folder

--- docker/READme.md
+++
@@ -1,1 +0,0 @@
-Run the php7.0 command like this ---> DOCKER_BUILDKIT=0 docker build -t myphp7.0 .
--- docker/nginx/docker/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-    echo "the code is updated & testing to see if the code is on the k8s pod";
-?>
